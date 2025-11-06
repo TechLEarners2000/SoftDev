59f98a50da691653e867dded2b392ba89f5e1362
TechLEarners2000 <techl.earners.offl@gmail.com>
2025-11-06 14:35:32 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://soft-dev-nqzn.vercel.app//api',
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
